84dc24e3dd41dfadb77d91f3fe7851e2715a3ed8
Roman Topchii <roman.topchii@dxc.com>
2023-05-11 10:53:53 +0300

    added implementation

--- TDDKata/FirstTDDSample/KataCalc.cs
+++ TDDKata/FirstTDDSample/KataCalc.cs
@@ -1,15 +1,25 @@
-﻿namespace FirstTDDSample
+﻿using System.Text.RegularExpressions;
+
+namespace FirstTDDSample
 {
     public class KataCalc
     {
         public int Add(string numbers)
         {
+            var separators = new string[2] { "\n", "," };
+            var a = numbers.Count();
+            if (numbers.Length > 4 && numbers.StartsWith("//") && numbers[3].ToString() == "\n")
+            {
+                separators = new string[1] { numbers[2].ToString() };
+                numbers = numbers.Substring(4, numbers.Length - 4);
+            }
+
             if (numbers == string.Empty)
             {
                 return 0;
             }
 
-            var splitedStringNumbers = numbers.Split(new string[2] {"\n", ","}, StringSplitOptions.None);
+            var splitedStringNumbers = numbers.Split(separators, StringSplitOptions.None);
             var numberList = new List<int>();
             foreach (var stringNumber in splitedStringNumbers)
             {
