e0af65f08c2f69c4a8edabbf3dd823328b3c6e00
Roman Topchii <roman.topchii@dxc.com>
2023-05-12 09:59:21 +0300

    - added tests fro "Delimiters can be of any length with the following format:  “//[delimiter]\n” for example: “//[***]\n1***2***3”  should return 6" - rename test methods with name convenience

--- TDDKata/TDDKata/KataCalcTest.cs
+++ TDDKata/TDDKata/KataCalcTest.cs
@@ -5,7 +5,7 @@ namespace TDDKata
     public class KataCalcTest
     {
         [Fact]
-        public void EmptyString()
+        public void When_AddEmptyString_Expect_Zero()
         {
             // Arrange
             string numbers = string.Empty;
@@ -21,7 +21,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddSingleNumber()
+        public void When_AddSingleNumber_Expect_CorrectCalc()
         {
             // Arrange
             string numbers = "1";
@@ -37,7 +37,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddTwoNumbers()
+        public void When_AddNumbersSplitedWithComma_Expect_CorrectCalc()
         {
             // Arrange
             string numbers = "1,2";
@@ -53,7 +53,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddLetterReturnMinusOne()
+        public void When_AddSingleLetter_Expect_MinusOne()
         {
             // Arrange
             string numbers = "w";
@@ -69,7 +69,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddNumberAndLetterReturnMinusOne()
+        public void When_AddNumberAndLetterSplitedWithComma_Expect_MinusOne()
         {
             // Arrange
             string numbers = "1,w";
@@ -85,7 +85,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddLeterAndNumberReturnMinusOne()
+        public void When_AddLetterAndNumberSplitedWithComma_Expect_MinusOne()
         {
             // Arrange
             string numbers = "q,1";
@@ -101,7 +101,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddNumbersSplitedWithNewLine()
+        public void When_AddNumbersSplitedWithNewLine_Expect_CorrectCalc()
         {
             // Arrange
             string numbers = "1\n1";
@@ -117,7 +117,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddNumbersSplitedWithNewLineAndComma()
+        public void When_AddNumbersSplitedWithCommaAndNewLine_Expect_CorrectCalc()
         {
             // Arrange
             string numbers = "1\n2,3";
@@ -133,7 +133,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddNumbersSplitedWithNewLineAndCommaReturnMinusOne()
+        public void When_AddNumberAndLetterWithoutSpecifiedDelimeter_Expect_MinusOne()
         {
             // Arrange
             string numbers = "2,\n";
@@ -149,7 +149,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddNumbersWithSpecifiedDelimeter()
+        public void When_AddNumbersWithSpecifiedDelimeterSemicolon_Expect_CorrectCalc()
         {
             // Arrange
             string numbers = "//;\n1;2";
@@ -165,7 +165,7 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddNumbersAndLetterWithSpecifiedDelimeterReturnMinusOne()
+        public void When_AddNumbersAndLetterWithSpecifiedDelimeterSemicolon_Expect_MinusOne()
         {
             // Arrange
             string numbers = "//;\n1;A";
@@ -181,11 +181,107 @@ namespace TDDKata
         }
 
         [Fact]
-        public void AddLetterWithSpecifiedDelimeterReturnMinusOne()
+        public void When_AddLetterWithSpecifiedDelimeterSemicolon_Expect_MinusOne()
         {
             // Arrange
             string numbers = "//;\nA";
             var expected = -1;
+
+            var kataCalc = new KataCalc();
+
+            // Act
+            var actual = kataCalc.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void When_AddNumbersAndLetterWithSpecifiedDelimeterAsteriskDollarPersent_Expect_CorrectCalc()
+        {
+            // Arrange
+            string numbers = "//[*$%]\n1*$%2*$%3";
+            var expected = 6;
+
+            var kataCalc = new KataCalc();
+
+            // Act
+            var actual = kataCalc.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void When_AddNumbersAndLetterWithSpecifiedDelimeterAsteriskDollarPersent_Expect_MinusOne()
+        {
+            // Arrange
+            string numbers = "//[*$%]\n1*$%A";
+            var expected = -1;
+
+            var kataCalc = new KataCalc();
+
+            // Act
+            var actual = kataCalc.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void When_AddLetterWithSpecifiedDelimeterMultipleAsterisks_Expect_MinusOne()
+        {
+            // Arrange
+            string numbers = "//[***]\nA";
+            var expected = -1;
+
+            var kataCalc = new KataCalc();
+
+            // Act
+            var actual = kataCalc.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void When_AddNumbersWithSpecifiedDelimeterMultipleSquareBrackets_Expect_CorrectCalc()
+        {
+            // Arrange
+            string numbers = "//[]]]]\n1]]]2]]]3";
+            var expected = 6;
+
+            var kataCalc = new KataCalc();
+
+            // Act
+            var actual = kataCalc.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void When_AddNumbersWithSpecifiedDelimeterNewLines_Expect_CorrectCalc()
+        {
+            // Arrange
+            string numbers = "//[\n\n\n]\n1\n\n\n2\n\n\n3";
+            var expected = 6;
+
+            var kataCalc = new KataCalc();
+
+            // Act
+            var actual = kataCalc.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void When_AddNumbersWithSpecifiedDelimeterSquareBracketAndNewLine_Expect_CorrectCalc()
+        {
+            // Arrange
+            string numbers = "//[]\n]\n1]\n2]\n3";
+            var expected = 6;
 
             var kataCalc = new KataCalc();
 
@@ -212,4 +308,13 @@ Remember to refactor after each passing test
 Allow the Add method to handle new lines between numbers(instead of commas)
 the following input is ok:  “1\n2,3”  (will equal 6)
 the following input is NOT ok:  “1,\n”
+*/
+
+/*Support different delimiters
+to change a delimiter, the beginning of the string will contain a separate line that looks like this:   “//[delimiter]\n[numbers…]” for example “//;\n1;2” should return three where the default delimiter is ‘;’ 
+the first line is optional – all existing scenarios should still be supported
+*/
+
+/*Delimiters can be of any length with the following format:  “//[delimiter]\n” for example: “//[***]\n1***2***3” 
+should return 6
 */
